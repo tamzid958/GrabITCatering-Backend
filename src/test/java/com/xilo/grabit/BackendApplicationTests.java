020b5361b9ef80b3dc02f558aecefdef98e1804d
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-05-10 13:59:13 +0600

    first commit

---
+++ src/test/java/com/xilo/grabit/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xilo.grabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
